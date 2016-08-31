2407a7b919067711974ea7544970e76d4789883a
Jacques Gangloff <jacques.gangloff@gmail.com>
2016-08-31 14:21:13 +0200

    Batch script example for XM 430.

--- batch.sh
+++ batch.sh
@@ -1,19 +1,54 @@
 #!/bin/sh
+# Example script for XM430
+
+## Script initialization
+# Non blocking stdin
+if [ -t 0 ]; then stty -echo -icanon -icrnl time 0 min 0; fi
 # Batch of instructions to be sent to Dynamixel devices
 DXL_PORTNAME=/dev/ttyUSB0
 DXL_BAUDRATE=1000000
 DXL_STARTID=1
 DXL_NBID=1
 DXL_PROTO=2
+DXL_POS1=-511
+DXL_POS2=511
 
-# Initialization sequence
+## Dynamixel initialization sequence
+# Operating mode 4: extende position control
+./dxl write $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 11 1 4
+# Position D gain
+./dxl write $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 80 2 0
+# Position I gain
+./dxl write $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 82 2 0
+# Position P gain
+./dxl write $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 84 2 800
+# Position FF2 gain
+./dxl write $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 88 2 0
+# Position FF1 gain
+./dxl write $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 90 2 0
+# Torque enable
+./dxl write $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 64 1 1
 
-# Loop
-while [ 1 ]
+## Loop
+echo "Press any key to exit..."
+keypress=''
+while [ "x$keypress" = "x" ];
 do
-	./dxl write $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 65 1 0
-	sleep 0.1
-	./dxl write $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 65 1 1
-	sleep 0.1
-	./dxl read $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 120 2 0
+	# Go to position 1
+	./dxl write $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 116 4 $DXL_POS1
+	sleep 0.5
+	# Read position
+	./dxl read $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 132 4 1
+	# Go to position 2
+	./dxl write $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 116 4 $DXL_POS2
+	sleep 0.5
+	# Read position
+	./dxl read $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 132 4 1
+	keypress="`cat -v`"
 done
+
+## Cleanup
+# Torque disable
+./dxl write $DXL_PORTNAME $DXL_BAUDRATE $DXL_STARTID $DXL_NBID $DXL_PROTO 64 1 0
+# Restore tty behavior
+if [ -t 0 ]; then stty sane; fi
